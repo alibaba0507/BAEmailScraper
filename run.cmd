bbdad7bf3f930a36befbd6f5b6d4f66800c02f36
alibaba0507 <alibaba0507@gmail.com>
2023-08-03 11:02:15 +0300

    Init
    init

--- run.cmd
+++ run.cmd
@@ -1,6 +1,7 @@
 echo OFF
 pause
 rem node app.js -q "+site:docs.google.co.uk \"@gmail.com\" \"@live.com\" \"@yahoo.com\" \"@hotmail.com\"" -f docs.google.co.uk.txt
-node app.js -q "gmail bitcoin btc filetype:txt" -f bitcoin_2.txt
+rem node app.js -q "gmail bitcoin btc filetype:txt" -f bitcoin_2.txt
+node app.js -q "@ gmail|yahoo|aol|msn|hotmail keto|diet filetype:txt OR filetype:csv" -f keto_diet_3.txt
 rem node app.js -q "View the profile @ gmail|yahoo|aol|msn|hotmail bitcoin" -f bitcoin_1.txt --rgex "\S+\s*@\s*\S+"
 pause
