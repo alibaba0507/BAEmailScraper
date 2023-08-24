67379e731a9f2e35b92fb2b3d86de0e02e2186d9
alibaba0507 <alibaba0507@gmail.com>
2023-08-24 17:10:54 +0300

    init

--- run.cmd
+++ run.cmd
@@ -3,5 +3,5 @@ pause
 rem node app.js -q "+site:docs.google.co.uk \"@gmail.com\" \"@live.com\" \"@yahoo.com\" \"@hotmail.com\"" -f docs.google.co.uk.txt
 rem node app.js -q "gmail bitcoin btc filetype:txt" -f bitcoin_2.txt
 rem node app.js -q "@ gmail|yahoo|aol|msn|hotmail keto|diet filetype:txt OR filetype:csv" -f keto_diet_3.txt
-node app.js -q "Keto diet foods to avoid" --articles 1
+node app.js -q "What are the basic rules for keto" --articles 1
 pause
